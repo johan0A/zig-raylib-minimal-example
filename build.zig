c0d49314f8472599148771466f1e0861d4ea7d5f
johan0A <arnaudchesse@outlook.com>
2024-10-14 22:09:26 +0200

    added code and comments

--- build.zig
+++ build.zig
@@ -6,12 +6,15 @@ pub fn build(b: *B) void {
     const optimize = b.standardOptimizeOption(.{});
 
     {
+        // this is for compiling the executable
+
         const exe = b.addExecutable(.{
             .name = "zig-exe-template",
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
         });
+        addDependencies(exe, b, target, optimize);
 
         b.installArtifact(exe);
         const run_cmd = b.addRunArtifact(exe);
@@ -21,16 +24,20 @@ pub fn build(b: *B) void {
             run_cmd.addArgs(args);
         }
 
+        // and then maybe run it
         const run_step = b.step("run", "Run the app");
         run_step.dependOn(&run_cmd.step);
     }
 
     {
+        // this is for running the tests
+
         const exe_unit_tests = b.addTest(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
         });
+        addDependencies(exe_unit_tests, b, target, optimize);
 
         const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
         const test_step = b.step("test", "Run unit tests");
@@ -38,12 +45,15 @@ pub fn build(b: *B) void {
     }
 
     {
+        // this is for running static checks for zls
+        // more info: https://kristoff.it/blog/improving-your-zls-experience/
         const exe_check = b.addExecutable(.{
             .name = "check",
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
         });
+        addDependencies(exe_check, b, target, optimize);
 
         const tests_check = b.addTest(.{
             .name = "check",
@@ -51,9 +61,10 @@ pub fn build(b: *B) void {
             .target = target,
             .optimize = optimize,
         });
+        addDependencies(tests_check, b, target, optimize);
 
         const check = b.step("check", "Check if exe and tests compile");
-        check.dependOn(&exe_check.step);
+        // check.dependOn(&exe_check.step);
         check.dependOn(&tests_check.step);
     }
 }
@@ -64,8 +75,20 @@ fn addDependencies(
     target: B.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
 ) void {
-    _ = compile_step;
-    _ = b;
-    _ = target;
-    _ = optimize;
+    // we are going to setup out dependencies here
+
+    // here we get the depency
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // here we import the raylib module
+    const raylib = raylib_dep.module("raylib");
+    compile_step.root_module.addImport("raylib", raylib);
+
+    // here we link against the raylib c artifact
+    // this is not so usual for zig packages but I guess we can use our own version of raylib if we want
+    const raylib_artifact = raylib_dep.artifact("raylib");
+    compile_step.linkLibrary(raylib_artifact);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
+const rl = @import("raylib");
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+    }
 }
