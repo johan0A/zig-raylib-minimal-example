aab63fe5b3b65e1ade44f32e41589cc23e9e9959
johan0A <arnaudchesse@outlook.com>
2025-03-19 17:10:16 +0100

    update to 0.14.0 and added debugging

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-A minimal example of how to use the raylib bindings from https://github.com/Not-Nik/raylib-zig
+A minimal example of how to use the raylib bindings from https://github.com/Not-Nik/raylib-zig for zig 0.14.0
 
 with comments explaining the code.
+
+### debugging:
+Debugging is setup for vscode and lldb (you need to download the lldb extension for it to work)
+on windows set the LLDB_USE_NATIVE_PDB_READER environment variable to 1 for debugging to work.
+
+for debbuging with self hosted backend see: https://github.com/ziglang/zig/wiki/LLDB-for-Zig
--- build.zig
+++ build.zig
@@ -1,93 +1,63 @@
 const std = @import("std");
-const B = std.Build;
 
-pub fn build(b: *B) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const name = b.option([]const u8, "name", "set the name of the emitted binary"); // this is mostly for making debugging with vscode simpler
 
-    {    // this is for compiling the executable
-        
-        const exe = b.addExecutable(.{
-            .name = "zig-exe-template",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(exe, b, target, optimize);
-
-        b.installArtifact(exe);
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        // and then maybe run it
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
-    }
-
-    {    // this is for running the tests
-
-        const exe_unit_tests = b.addTest(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(exe_unit_tests, b, target, optimize);
-
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&run_exe_unit_tests.step);
-    }
-
-    {   // this is for letting zls run static checks
-        // more info: https://kristoff.it/blog/improving-your-zls-experience/
-
-        const exe_check = b.addExecutable(.{
-            .name = "check",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(exe_check, b, target, optimize);
-
-        const tests_check = b.addTest(.{
-            .name = "check",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDependencies(tests_check, b, target, optimize);
-
-        const check = b.step("check", "Check if exe and tests compile");
-        check.dependOn(&exe_check.step);
-        check.dependOn(&tests_check.step);
-    }
-}
-
-fn addDependencies(
-    compile_step: *B.Step.Compile,
-    b: *B,
-    target: B.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-) void {
-    // we are going to setup our dependencies here
+    const root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     // here we get the dependency
-    const raylib_dep = b.dependency("raylib-zig", .{
+    const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
     });
 
     // here we import the raylib module
-    const raylib = raylib_dep.module("raylib");
-    compile_step.root_module.addImport("raylib", raylib);
+    root_module.addImport("raylib", raylib_dep.module("raylib"));
 
-    // here we link against the raylib c artifact,
+    // here we link against the raylib library artifact,
     // it is needed here but this is not very usual for zig packages.
     // I guess it is usefull if we want to use our own version of raylib.
-    const raylib_artifact = raylib_dep.artifact("raylib");
-    compile_step.linkLibrary(raylib_artifact);
+    root_module.linkLibrary(raylib_dep.artifact("raylib"));
+
+    { // this is for compiling the executable
+        const exe = b.addExecutable(.{ .name = name orelse "zig-exe-template", .root_module = root_module });
+        b.installArtifact(exe);
+
+        // and then maybe run it
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| run_cmd.addArgs(args);
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    { // this is for running the tests
+        const tests = b.addTest(.{ .name = name orelse "test", .root_module = root_module });
+
+        const run_tests = b.addRunArtifact(tests);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_tests.step);
+
+        const debug_tests_artifact = b.addInstallArtifact(tests, .{});
+        const debug_tests_step = b.step("build-test", "Create a test artifact that runs the tests");
+        debug_tests_step.dependOn(&debug_tests_artifact.step);
+    }
+
+    { // this is for letting zls run static checks
+        // more info: https://kristoff.it/blog/improving-your-zls-experience/ (the code in the blog post is outdated)
+        const exe_check = b.addExecutable(.{ .name = "check", .root_module = root_module });
+        const tests_check = b.addTest(.{ .name = "check", .root_module = root_module });
+
+        const check = b.step("check", "Check if exe and tests compile");
+        check.dependOn(&exe_check.step);
+        check.dependOn(&tests_check.step);
+    }
 }
